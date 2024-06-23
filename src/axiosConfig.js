4ccf65f1263751172126ee5bf4ec001a02db679c
Amsale-Tesfaye <162537912+Amsale-Tesfaye@users.noreply.github.com>
2024-06-23 15:11:16 -0400

    Adding-basing-url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5500/api"
-})
+  // baseURL: "http://localhost:5500/api"
+  baseURL: "https://evangadi-forum-backend-1-luva.onrender.com"
+});
 
 export default axiosBase
